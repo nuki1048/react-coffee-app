8869a99257c1c00ec6d1fb616cc9cfcb9a06bcd8
Yur1 <nik456618@gmail.com>
2023-02-26 02:29:23 +0200

    fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
 		<div>
 			<Header tittle={"Our Coffee"} path={"img/second_main_bg.jpg"} />
 			<AboutComponent title={"About our beans"} path={"img/aboutPhoto.jpg"} />
-			<hr />
+			<hr style={{ width: "240px", marginTop: "60px" }} />
 			<Footer />
 		</div>
 	);
