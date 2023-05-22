c4114fe62b90b2274cc41056f0da65f2560cbc5d
nuki1048 <112338685+nuki1048@users.noreply.github.com>
2023-05-22 15:04:07 +0300

    Fix

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./ProductItem.scss";
+
+import img from "../../assets/img/cards_preview/cofffee-presto.png";
 const ProductItem = ({ name, price }) => {
-	return (
-		<div className="product-item">
-			<img src="img/cards_preview/cofffee-presto.png" alt="" />
-			<h3 className="product-info">{name}</h3>
-			<div className="product-price">{price}$</div>
-		</div>
-	);
+  return (
+    <div className="product-item">
+      <img src={img} alt="" />
+      <h3 className="product-info">{name}</h3>
+      <div className="product-price">{price}$</div>
+    </div>
+  );
 };
 
 export default ProductItem;
--- src/components/StoreItem/StoreItem.jsx
+++ src/components/StoreItem/StoreItem.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./StoreItem.scss";
+import img from "../../assets/img/cards_preview/coffee-arabica.png";
 const StoreItem = ({ country, name, price, path, id }) => {
-	return (
-		<Link to={`/ourCoffee/${id}`} className="store-item">
-			<img src="img/cards_preview/coffee-arabica.png" alt={country} />
-			<h3 className="store-item_name">{name}</h3>
-			<div className="store-item_sort">{country}</div>
-			<div className="store-item_price">{price}$</div>
-		</Link>
-	);
+  return (
+    <Link to={`/ourCoffee/${id}`} className="store-item">
+      <img src={img} alt={country} />
+      <h3 className="store-item_name">{name}</h3>
+      <div className="store-item_sort">{country}</div>
+      <div className="store-item_price">{price}$</div>
+    </Link>
+  );
 };
 
 export default StoreItem;
